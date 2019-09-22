dac25bc797cf7494cf3c939e7dbbeb6894693550
johnny <j.vitor.mf@gmail.com>
2019-09-22 20:39:40 -0300

    Dados do Header

--- src/pages/SingUp/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-export default function SingUp() {
-    return <h1>SignUp</h1>;
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SingIn from '../pages/SignIn';
-import SingUp from '../pages/SingUp';
+import SingIn from '~/pages/SignIn';
+import SingUp from '~/pages/SignUp';
 
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
     return (

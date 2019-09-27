fbe82c05c17bf29f5d56c74352c0cb33f4aaa992
johnny <j.vitor.mf@gmail.com>
2019-09-26 22:00:40 -0300

    Frontend completo

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -15,6 +16,11 @@ export default function Profile() {
     function handleSubmit(data) {
         dispatch(updateProfileRequest(data));
     }
+
+    function handleSignOut() {
+        dispatch(signOut());
+    }
+
     return (
         <Container>
             <Form initialData={profile} onSubmit={handleSubmit}>
@@ -47,7 +53,9 @@ export default function Profile() {
                 <button type="submit">Atualizar perfil</button>
             </Form>
 
-            <button type="button">Sair do GoBarber</button>
+            <button type="button" onClick={handleSignOut}>
+                Sair do GoBarber
+            </button>
         </Container>
     );
 }
